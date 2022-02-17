c85bcb4daac98e463acb7cde6ccfb4c68a74852f
Philippe <philippe@igor-brevnjovski.net>
2022-02-17 18:20:29 +0100

    Before Feign

--- src/main/java/guru/sfg/beer/inventory/service/web/controllers/BeerInventoryController.java
+++ src/main/java/guru/sfg/beer/inventory/service/web/controllers/BeerInventoryController.java
@@ -27,10 +27,14 @@ public class BeerInventoryController {
     @GetMapping("api/v1/beer/{beerId}/inventory")
     List<BeerInventoryDto> listBeersById(@PathVariable UUID beerId){
         log.debug("Finding Inventory for beerId:" + beerId);
-
-        return beerInventoryRepository.findAllByBeerId(beerId)
+        
+        List<BeerInventoryDto> listBeerInventory = beerInventoryRepository.findAllByBeerId(beerId)
                 .stream()
                 .map(beerInventoryMapper::beerInventoryToBeerInventoryDto)
                 .collect(Collectors.toList());
+        
+        log.debug("Got Inventory for beerId:" + listBeerInventory);
+
+        return listBeerInventory;
     }
 }
--- src/main/java/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapper.java
+++ src/main/java/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapper.java
@@ -1,8 +1,9 @@
 package guru.sfg.beer.inventory.service.web.mappers;
 
+import org.mapstruct.Mapper;
+
 import guru.sfg.beer.inventory.service.domain.BeerInventory;
 import guru.sfg.brewery.model.BeerInventoryDto;
-import org.mapstruct.Mapper;
 
 /**
  * Created by jt on 2019-05-31.
